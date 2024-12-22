e1d01786c1d89a5f8ba707bb596c36590c9a3ae7
Evann Jallet <evann.jallet@gmail.com>
2024-12-22 21:33:02 +0100

    First commit

---
+++ Plugin.cs
@@ -0,0 +1,21 @@
+﻿using System.Globalization;
+using BepInEx;
+using BepInEx.Logging;
+
+namespace UnityLocaleFix;
+
+[BepInPlugin(MyPluginInfo.PLUGIN_GUID, MyPluginInfo.PLUGIN_NAME, MyPluginInfo.PLUGIN_VERSION)]
+public class Plugin : BaseUnityPlugin
+{
+    internal static new ManualLogSource Logger;
+        
+    private void Awake()
+    {
+        Logger = base.Logger;
+        Logger.LogInfo($"Plugin {MyPluginInfo.PLUGIN_GUID} is loaded!");
+        
+        CultureInfo.DefaultThreadCurrentCulture = CultureInfo.InvariantCulture;
+        CultureInfo.DefaultThreadCurrentUICulture = CultureInfo.InvariantCulture;
+        Logger.LogInfo("Culture set to invariant globally.");
+    }
+}
